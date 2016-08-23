bddf65fa4004f312adf813009bf4554bb869dacb
orcuslc <orcuslc@hotmail.com>
2016-08-23 23:53:55 +0800

    Add a script to normalize data

---
+++ scripts/daily_dist/normalize.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+def normalize(x):
+	return np.asarray(x)/max(x)
+
+	
--- scripts/data_prep/prep_weather.py
+++ scripts/data_prep/prep_weather.py
@@ -3,3 +3,5 @@ import pandas as pd
 raw_weather_root = '../../Raw-Data/Weather/shanghai.csv'
 weather = pd.read_csv(raw_weather_root, encoding='gbk'
 	, index_col='date')
+index = weather.index
+for i in weather.loc[index].
