5aa368874ba67e8e2b4c6384b423a240a5cf3418
orcuslc <orcuslc@hotmail.com>
2016-08-23 23:57:03 +0800

    add a function to get time list

--- scripts/daily_dist/normalize.py
+++ scripts/daily_dist/normalize.py
@@ -5,4 +5,8 @@ import datetime as dt
 def normalize(x):
 	return np.asarray(x)/max(x)
 
-	
+def get_time_list(start):
+	td = dt.datetime(2001, 1, 1, 0, 0, 30) - dt.datetime(2001, 1, 1, 0, 0, 0)
+
+if __name__ == '__main__':
+	get_time_list(1)
