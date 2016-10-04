30a5282c08a1c65a1d20635ffaeef463d8be91f4
orcuslc <orcuslc@hotmail.com>
2016-10-04 23:36:52 +0800

    add some functions

--- svm-1.0.0/src/conf.py
+++ svm-1.0.0/src/conf.py
@@ -1,5 +1,8 @@
 # Configurations
 
+###############################
+# For prep_attr.py
+###############################
 LOAD_SCALE = 10000
 TEMP_SCALE = 29
 DAYS_BACK = 6
@@ -16,3 +19,14 @@ WEEKDAYS = ['0 0 0 0 0 1',
 			'0 0 0 0 0 0']
 
 HOLIDAYS = ['1', '0.21', '0']
+
+################################
+# For predict.py
+################################
+
+# Notice: Do not change values in this region!
+C = 4
+gamma = 2**-4
+
+# Ordinary configurations
+MODEL_PATH = '../data/'
--- svm-1.0.0/src/predict.py
+++ svm-1.0.0/src/predict.py
@@ -16,27 +16,62 @@ class Predictor:
 	Private:
 		self._training_data : matrix-like
 			training data, each line is a record point, the first of which is target, and 2:n is the attributes.
+
+		self._testing_data : matrix-like, maybe empty
+			testing data, each line is a record point, the first of which is target, and 2:n is the attributes.
 		
-		self._x: matrix-like
+		self._training_x: matrix-like
 			training data, the attributes.
 
-		self._y : matrix-like
+		self._training_y : matrix-like
 			training data, the targets.
 
-	Public:
+		self._testing_x : matrix-like, maybe empty
+			testing data, the attributes
 
+		self._testing_y : matrix-like, maybe empty
+			testing data, the target
 
-	Methods
-	-------
 	Public:
+		self.clf : SVR model
+			generated and trained by self.train()
+
+		self.
 
 	"""
-	def __init__(self, attr_path):
-		self._training_data = self._read(attr_path)
-		self._y = [float(record[0]) for record in self.training_data]
-		self._x = [str2float(record[1:]) for record in self._training_data]
+	def __init__(self, attr_path, n):
+		"""
+		Initialize self.
 
-	def _read(self, attr_path):
+		Parameters
+		----------
+		attr_path : string
+			The path of attributes.
+
+		n : int
+			The number of record points;
+			The first n points are used as training data, and the latter points as testing data(if exists)
+
+		Returns of self._read()
+		-------
+		data : matrix-like
+			Each line of `data` is a record point.
+			The first n rows of data is regarded as training data, the n:-1 rows of data as testing data.
+			If n == len(data) - 1:
+				we regard test data as an empty list.
+
+		Raises
+		------
+		IndexError:
+			If n >= len(data).
+		"""
+		self._training_data, self._testing_data = self._read(attr_path, n)
+		self._training_y = [float(record[0]) for record in self._training_data]
+		self._training_x = [str2float(record[1:]) for record in self._training_data]
+		self._testing_x = [str2float(record[1:]) for record in self._testing_data]
+		self._testing_y = [float(record[0]) for record in self._testing_data]
+
+	def _read(self, attr_path, n):
 		"""
 		Read attributes from files;
 		Private function, used only in self.__init__()
@@ -46,20 +81,47 @@ class Predictor:
 		attr_path : string
 			The path of attributes.
 
+		n : int
+			The number of training data points.
+
 		Returns
 		-------
 		data : matrix-like
 			Each line of `data` is a record point.
+			The first n rows of data is regarded as training data, the n:-1 rows of data as testing data.
+			If n == len(data) - 1:
+				we regard test data as an empty list.
 
 		Raises
 		------
+		IndexError:
+			If n >= len(data).
 		"""
 		with open(attr_path) as f:
 			data = f.read().split('\n')
 		for index in range(len(data)):
 			data[index] = data[index].split(' ')[:-1]
-		return data[:-1]
+		if n >= len(index):
+			raise IndexError("n must be less than the number of record points.")
+		return data[:n], data[n:-1]
 
 	def train(self):
-		
+		"""
+		Generate and train the model with SVR.
+		The model is saved as self.clf
+
+		Parameters
+		----------
+		No input parameters need.
+
+		Returns
+		-------
+		No returns.
+		"""
+		self.clf = SVR(C = C, gamma = gamma)
+		self.clf.fit(self._training_x, self._training_y)
+
+	def predict(self, x = None, y = None):
+		"""
+		Predict 
 
