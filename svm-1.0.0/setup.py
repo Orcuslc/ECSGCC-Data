23ebdf443c8dfe9bba9c565862bddb59cfc87bb5
Orcuslc <orcuslc@hotmail.com>
2016-11-15 15:03:59 +0800

    SVR

---
+++ svm-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='SVM',
+		version='1.0.0',
+		description="SVM for Load Forecasting",
+		author="Orcuslc",
+		author_email="orcuslc@hotmail.com",
+		url="https://github.com/orcuslc/ecsgcc-data",
+		packages=['', 'src', 'util'])
